c40c4ffea88e15345b10ec0fb379933595e03de5
Gardner Bickford <gardner@quickture.com>
2026-02-22 15:36:34 +1300

    Remove boilerplate task API endpoints and unused types

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,6 @@ import { fromHono } from "chanfana";
 import { Hono } from "hono";
 import { CelestialCalendar } from "./endpoints/celestialCalendar";
 import { SunriseCalendar } from "./endpoints/sunriseCalendar";
-import { TaskCreate } from "./endpoints/taskCreate";
-import { TaskDelete } from "./endpoints/taskDelete";
-import { TaskFetch } from "./endpoints/taskFetch";
-import { TaskList } from "./endpoints/taskList";
 
 // Start a Hono app
 const app = new Hono<{ Bindings: Env }>();
@@ -18,13 +14,6 @@ const openapi = fromHono(app, {
 // Register OpenAPI endpoints
 openapi.get("/api/celestial", CelestialCalendar);
 openapi.get("/api/sunrise", SunriseCalendar);
-openapi.get("/api/tasks", TaskList);
-openapi.post("/api/tasks", TaskCreate);
-openapi.get("/api/tasks/:taskSlug", TaskFetch);
-openapi.delete("/api/tasks/:taskSlug", TaskDelete);
-
-// You may also register routes for non OpenAPI directly on Hono
-// app.get('/test', (c) => c.text('Hono!'))
 
 // Export the Hono app
 export default app;
